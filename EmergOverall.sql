5296d3dc01f88080b76de328586fd8206549a3ac
John Paul B Dala <jaypeedala31@gmail.com>
2022-11-04 15:36:03 +0800

    update emerg overall SP

--- EmergOverall.sql
+++ EmergOverall.sql
@@ -1,35 +1,40 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `EmergOverall`(
+userIDVal varchar(255),
 NodeTypeOne varchar(255),
-NodeTypeTwo varchar(255),
-userIDVal varchar(255)
+NodeTypeTwo varchar(255)
 )
 BEGIN
 	
     
-DECLARE MonitoredDevices INT;
-DECLARE OfflineDevices INT;
-DECLARE OperationalDevices INT;
-DECLARE LowBattery INT;
-DECLARE AlertDevicesExitEmerg INT;
-DECLARE AlertDevices INT;
+	DECLARE MonitoredDevices INT;
+	DECLARE OfflineDevices INT;
+	DECLARE OperationalDevices INT;
+	DECLARE LowBattery INT;
+	DECLARE AlertDevicesExitEmerg INT;
+	DECLARE AlertDevices INT;
  
-SET MonitoredDevices = (Select count(*) As MonitoredDevices from node_details where NodeType In (NodeTypeOne, NodeTypeTwo) and NetworkID in (SELECT NetworkID FROM users_network where UserID = userIDVal));
-SET OfflineDevices = (Select count(*) As OfflineDevices from node_details where NodeType In (NodeTypeOne, NodeTypeTwo) and NodeOnlineStatus = 0 and NetworkID in (SELECT NetworkID FROM users_network where UserID = userIDVal));
-SET AlertDevicesExitEmerg = (Select count(distinct alarm.NodeID) As LowBattery from node_details As n JOIN node_alarm_log As alarm 
-on n.NodeID = alarm.NodeID and n.NetworkID = alarm.NetworkID and n.NodeType In (NodeTypeOne, NodeTypeTwo) 
-and n.NetworkID in (SELECT NetworkID FROM users_network where UserID = userIDVal)
-where alarm.IsResolved is null
-and n.NetworkID in (SELECT NetworkID FROM users_network where UserID = userIDVal)
-and alarm.NetworkID in (SELECT NetworkID FROM users_network where UserID = userIDVal));
-SET LowBattery = (Select count(distinct alarm.NodeID) As LowBattery from node_details As n JOIN node_alarm_log As alarm 
-on n.NodeID = alarm.NodeID and n.NetworkID = alarm.NetworkID and n.NodeType In (NodeTypeOne, NodeTypeTwo) 
-and n.NetworkID in (SELECT NetworkID FROM users_network where UserID = userIDVal)
-where alarm.IsResolved is null
-and n.NetworkID in (SELECT NetworkID FROM users_network where UserID = userIDVal)
-and alarm.NetworkID in (SELECT NetworkID FROM users_network where UserID = userIDVal)
-and alarm.Descr = 'Low Battery');
-SET AlertDevices = AlertDevicesExitEmerg + LowBattery;
-SET OperationalDevices = MonitoredDevices - (OfflineDevices + AlertDevices);
-Select MonitoredDevices As MonitoredDevices, OperationalDevices As OperationalDevices, AlertDevices As AlertDevices, OfflineDevices As OfflineDevices, LowBattery As LowBattery; 
+	SET MonitoredDevices = (Select count(*) As MonitoredDevices from node_details where NodeType In (NodeTypeOne, NodeTypeTwo) and NetworkID in (SELECT NetworkID FROM users_network where UserID = userIDVal));
+	SET OfflineDevices = (Select count(*) As OfflineDevices from node_details where NodeType In (NodeTypeOne, NodeTypeTwo) and NodeOnlineStatus = 0 and NetworkID in (SELECT NetworkID FROM users_network where UserID = userIDVal));
+	SET AlertDevicesExitEmerg = (Select count(distinct alarm.NodeID) As LowBattery from node_details As n JOIN node_alarm_log As alarm 
+		on n.NodeID = alarm.NodeID and n.NetworkID = alarm.NetworkID and n.NodeType In (NodeTypeOne, NodeTypeTwo) 
+		and n.NetworkID in (SELECT NetworkID FROM users_network where UserID = userIDVal)
+		where alarm.IsResolved is null
+		and n.NetworkID in (SELECT NetworkID FROM users_network where UserID = userIDVal)
+		and alarm.NetworkID in (SELECT NetworkID FROM users_network where UserID = userIDVal));
+        
+	SET LowBattery = (Select count(distinct alarm.NodeID) As LowBattery from node_details As n JOIN node_alarm_log As alarm 
+		on n.NodeID = alarm.NodeID and n.NetworkID = alarm.NetworkID and n.NodeType In (NodeTypeOne, NodeTypeTwo) 
+		and n.NetworkID in (SELECT NetworkID FROM users_network where UserID = userIDVal)
+		where alarm.IsResolved is null
+		and n.NetworkID in (SELECT NetworkID FROM users_network where UserID = userIDVal)
+		and alarm.NetworkID in (SELECT NetworkID FROM users_network where UserID = userIDVal)
+		and alarm.Descr = 'Low Battery');
+        
+	SET AlertDevices = AlertDevicesExitEmerg;
+    SET OperationalDevices = CASE WHEN 
+                            (MonitoredDevices > (OfflineDevices + AlertDevices)) THEN (MonitoredDevices - (OfflineDevices + AlertDevices))
+                            ELSE 0
+                        END;
+	Select MonitoredDevices As MonitoredDevices, OperationalDevices As OperationalDevices, AlertDevices As AlertDevices, OfflineDevices As OfflineDevices, LowBattery As LowBattery; 
 
 END
