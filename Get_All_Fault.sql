277e0de8e4aebead3e97af8531a2bf51bd60012f
John Paul B Dala <jaypeedala31@gmail.com>
2022-11-08 12:59:39 +0800

    update get all fault SP

--- Get_All_Fault.sql
+++ Get_All_Fault.sql
@@ -1,5 +1,6 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `Get_All_Fault`(
 	userIDVal varchar(255),
+	timezoneOffset varchar(255),
 	NodeTypeValOne varchar(255),
 	NodeTypeValTwo varchar(255),
 	NodeTypeValThree varchar(255)
@@ -9,7 +10,7 @@ BEGIN
 	SELECT distinct n.NodeName, n.NodeID, n.BuildingName As Building, n.BuildingLevel As Level, n.SectorName As Sector,
 	n.ServiceID, n.NodeType, alarm.Descr As FaultDesc, alarm.ResolvedDescription As ResolvedDesc,
     DATE_FORMAT(CONVERT_TZ(alarm.created_at, '+00:00', timezoneOffset), '%Y-%m-%d %H:%i:%s') AS CreatedDate,
-	alarm.created_at As CreatedDate, ifnull(DATE_FORMAT(CONVERT_TZ(alarm.ResolvedTime, '+00:00', timezoneOffset), '%Y-%m-%d %H:%i:%s'),'-') As ResolvedDate
+	ifnull(DATE_FORMAT(CONVERT_TZ(alarm.ResolvedTime, '+00:00', timezoneOffset), '%Y-%m-%d %H:%i:%s'),'-') As ResolvedDate
 	FROM node_details As n 
 	JOIN node_alarm_log As alarm on n.NodeID = alarm.NodeID and n.NetworkID = alarm.NetworkID
 	where n.NodeType In (NodeTypeValOne,NodeTypeValTwo,NodeTypeValThree) and alarm.IsResolved is null 
