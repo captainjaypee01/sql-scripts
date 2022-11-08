ddc3f164fc06591b7b5b0c724ffbccba3ca16944
John Paul B Dala <jaypeedala31@gmail.com>
2022-11-08 12:57:34 +0800

    update calc temp hotspot

--- Calc_Temperature_Hotspot.sql
+++ Calc_Temperature_Hotspot.sql
@@ -9,20 +9,20 @@ DECLARE cursor_Sector VARCHAR(255);
 DECLARE cursor_Val VARCHAR(255);
 DECLARE cursor_ValTwo VARCHAR(255);
 DECLARE done INT DEFAULT FALSE;
-DECLARE cursor_maximum CURSOR FOR SELECT n.BuildingName, n.SectorName, Max(SensorValue02) Maximum, Min(SensorValue02) Minimum FROM smartnew.node_latest_readings As reading 
-JOIN smartnew.node_details As n on n.NodeID = reading.NodeID and n.NetworkID = reading.NetworkID
+DECLARE cursor_maximum CURSOR FOR SELECT n.BuildingName, n.SectorName, Max(SensorValue02) Maximum, Min(SensorValue02) Minimum FROM node_latest_readings As reading 
+JOIN node_details As n on n.NodeID = reading.NodeID and n.NetworkID = reading.NetworkID
 group by n.BuildingName,n.SectorName, SensorValue02, n.NodeType, reading.updated_at, n.NetworkID having n.NodeType = 'FireExtinguisher'
-and n.NetworkID in (SELECT NetworkID FROM smartnew.users_network where UserID COLLATE utf8mb4_general_ci  = userIDVal);
+and n.NetworkID in (SELECT NetworkID FROM users_network where UserID COLLATE utf8mb4_general_ci  = userIDVal);
 
-DECLARE cursor_current CURSOR FOR SELECT n.BuildingName, n.SectorName, Max(SensorValue02) Maximum FROM smartnew.node_latest_readings As reading 
-JOIN smartnew.node_details As n on n.NodeID = reading.NodeID and n.NetworkID = reading.NetworkID
+DECLARE cursor_current CURSOR FOR SELECT n.BuildingName, n.SectorName, Max(SensorValue02) Maximum FROM node_latest_readings As reading 
+JOIN node_details As n on n.NodeID = reading.NodeID and n.NetworkID = reading.NetworkID
 group by n.BuildingName,n.SectorName, SensorValue02, n.NodeType, reading.updated_at, n.NetworkID 
-having n.NodeType = 'FireExtinguisher' and n.NetworkID in (SELECT NetworkID FROM smartnew.users_network where UserID COLLATE utf8mb4_general_ci  = userIDVal);
+having n.NodeType = 'FireExtinguisher' and n.NetworkID in (SELECT NetworkID FROM users_network where UserID COLLATE utf8mb4_general_ci  = userIDVal);
 
 DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
 
-TRUNCATE TABLE `smartnew`.`temperature_hotspot`;
-INSERT INTO `smartnew`.`temperature_hotspot`
+TRUNCATE TABLE `temperature_hotspot`;
+INSERT INTO `temperature_hotspot`
 	(`Location`,
 	`Sector`,
 	`minimum`,
@@ -31,8 +31,8 @@ INSERT INTO `smartnew`.`temperature_hotspot`
     `total_node`
     )
 SELECT BuildingName As Location, SectorName As Sector, 0,0,0,0
-FROM smartnew.node_details group by BuildingName, SectorName, NodeType, NetworkID having NodeType = 'FireExtinguisher'
-and NetworkID in (SELECT NetworkID FROM smartnew.users_network where UserID COLLATE utf8mb4_general_ci  = userIDVal);
+FROM node_details group by BuildingName, SectorName, NodeType, NetworkID having NodeType = 'FireExtinguisher'
+and NetworkID in (SELECT NetworkID FROM users_network where UserID COLLATE utf8mb4_general_ci  = userIDVal);
     	
 OPEN cursor_maximum;
   read_maximum_loop: LOOP
@@ -41,7 +41,7 @@ OPEN cursor_maximum;
       LEAVE read_maximum_loop;
     END IF;
     
-    UPDATE `smartnew`.`temperature_hotspot`
+    UPDATE `temperature_hotspot`
 	SET	`maximum` = cursor_Val, `minimum` = cursor_ValTwo
 	WHERE `Location` COLLATE utf8mb4_general_ci = cursor_BuildingName
     and `Sector` COLLATE utf8mb4_general_ci = cursor_Sector;
@@ -60,12 +60,12 @@ OPEN cursor_maximum;
       LEAVE read_current_loop;
     END IF;
     
-	UPDATE `smartnew`.`temperature_hotspot`
+	UPDATE `temperature_hotspot`
 	SET	`current_max` = cursor_Val
 	WHERE `Location` COLLATE utf8mb4_general_ci = cursor_BuildingName
     and `Sector` COLLATE utf8mb4_general_ci = cursor_Sector;
     
-    UPDATE `smartnew`.`temperature_hotspot`
+    UPDATE `temperature_hotspot`
 	SET	`average` =  (maximum + minimum) / 2
 	WHERE `Location` COLLATE utf8mb4_general_ci = cursor_BuildingName
     and `Sector` COLLATE utf8mb4_general_ci = cursor_Sector;
