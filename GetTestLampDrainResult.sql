693a4422bb4e98bcd2c617a2be1bae3ba09112ef
John Paul B Dala <jaypeedala31@gmail.com>
2022-11-11 11:09:46 +0800

    initial GetTestLampDrainResult SP

---
+++ GetTestLampDrainResult.sql
@@ -0,0 +1,15 @@
+CREATE DEFINER=`root`@`localhost` PROCEDURE `GetTestLampDrainResult`(
+TestTypeOne varchar(255),
+TestTypeTwo varchar(255),
+userIDVal varchar(255),
+ScheduleVal varchar(255)
+)
+BEGIN
+
+SELECT passed, failed, schedule_name, adho.test_finished_at test_finished_at,
+adho.test_started_at test_started_at,  adho.testType As testType, nodeType as teston, building As location
+FROM smartnew.exem_test_schedule As adho
+where adho.testType COLLATE utf8mb4_general_ci  in (TestTypeOne, TestTypeTwo);
+
+
+END
