f477c8555fad6a4cafd5843c6027f3a149a9582a
John Paul B Dala <jaypeedala31@gmail.com>
2022-11-11 11:10:27 +0800

    update GetTestLampDrainResult

--- GetTestLampDrainResult.sql
+++ GetTestLampDrainResult.sql
@@ -6,10 +6,10 @@ ScheduleVal varchar(255)
 )
 BEGIN
 
-SELECT passed, failed, schedule_name, adho.test_finished_at test_finished_at,
-adho.test_started_at test_started_at,  adho.testType As testType, nodeType as teston, building As location
-FROM smartnew.exem_test_schedule As adho
-where adho.testType COLLATE utf8mb4_general_ci  in (TestTypeOne, TestTypeTwo);
+    SELECT passed, failed, schedule_name, adho.test_finished_at test_finished_at,
+        adho.test_started_at test_started_at,  adho.testType As testType, nodeType as teston, building As location
+        FROM exem_test_schedule As adho
+        where adho.testType COLLATE utf8mb4_general_ci  in (TestTypeOne, TestTypeTwo);
 
 
 END
