1abee7f941c53daa99f0ded35a5de0f4f349b766
John Paul B Dala <jaypeedala31@gmail.com>
2022-11-11 18:38:49 +0800

    add GetEmergLuminariesAlertBreakdown SP

---
+++ GetEmergLuminariesAlertBreakdown.sql
@@ -0,0 +1,26 @@
+CREATE DEFINER=`root`@`localhost` PROCEDURE `GetEmergLuminariesAlertBreakdown`(
+userIDVal varchar(255),
+NodeTypeOne varchar(255),
+NodeTypeTwo varchar(255)
+)
+BEGIN
+
+	DECLARE LampFailure INT;
+	DECLARE DrainBatteryTestFailed INT;
+	
+	SET LampFailure = (Select count(*) As DrainBatteryTestFailed from node_details As n RIGHT JOIN node_alarm_log As alarm 
+		on n.NodeID = alarm.NodeID
+        and n.NodeType In (NodeTypeOne, NodeTypeTwo) 
+		and n.NetworkID in (SELECT NetworkID FROM users_network where UserID = userIDVal)
+		where n.Status = 'Active' 
+		and alarm.Descr like '%LampFailure%');
+
+	SET DrainBatteryTestFailed = (Select count(*) As DrainBatteryTestFailed from node_details As n RIGHT JOIN node_alarm_log As alarm 
+		on n.NodeID = alarm.NodeID
+        and n.NodeType In (NodeTypeOne, NodeTypeTwo) 
+		and n.NetworkID in (SELECT NetworkID FROM users_network where UserID = userIDVal)
+		where n.Status = 'Active' 
+		and alarm.Descr like '%DrainBatteryTestFailed%');
+        
+	Select DrainBatteryTestFailed As DrainBatteryTestFailed, LampFailure As LampFailure;
+END
--- GetTestScheduleLampDrainResult.sql
+++ GetTestScheduleLampDrainResult.sql
@@ -6,18 +6,18 @@ ScheduleVal varchar(255)
 )
 BEGIN
 
-SELECT details.nodeName as nodename, ex.state_label as testresults, details.nodeType as teston, details.BuildingName As location, details.BuildingLevel As level, details.SectorName As sector, 
-CONVERT_TZ(adnode.test_finished_at,'+00:00','+8:00') As test_finished_at,
-CONVERT_TZ(adho.next_schedule_date,'+00:00','+8:00') As next_schedule_date
-FROM smartnew.exem_test_schedule As adho 
-JOIN smartnew.exem_test_result As adnode on adnode.schedule_no = adho.id and adnode.test_type = testType
-JOIN (SELECT nodeName, max(updated_at) As max_updated_at FROM (select * from smartnew.exem_test_result where schedule = 1) As maxup group by nodeName) As lastOne
-on adnode.nodeName =  lastOne.nodeName and adnode.updated_at = lastOne.max_updated_at
-JOIN smartnew.ex_em_state ex on ex.state = adnode.adhoclamp_status and ex.test_type = testType
-JOIN smartnew.node_details As details on details.NodeID = adnode.nodeID and details.NetworkID = adnode.networkID
-where adnode.adhoclamp_status in ('f','8') and
-adnode.schedule = 1 and adho.testType COLLATE utf8mb4_general_ci  in (TestTypeOne, TestTypeTwo)
-and adnode.NetworkID COLLATE utf8mb4_general_ci in (SELECT NetworkID FROM smartnew.users_network where UserID COLLATE utf8mb4_general_ci  = userIDVal)
-order by details.nodeName, adnode.test_finished_at desc;
+    SELECT details.nodeName as nodename, ex.state_label as testresults, details.nodeType as teston, details.BuildingName As location, details.BuildingLevel As level, details.SectorName As sector, 
+        CONVERT_TZ(adnode.test_finished_at,'+00:00','+8:00') As test_finished_at,
+        CONVERT_TZ(adho.next_schedule_date,'+00:00','+8:00') As next_schedule_date
+        FROM exem_test_schedule As adho 
+        JOIN exem_test_result As adnode on adnode.schedule_no = adho.id and adnode.test_type = testType
+        JOIN (SELECT nodeName, max(updated_at) As max_updated_at FROM (select * from exem_test_result where schedule = 1) As maxup group by nodeName) As lastOne
+        on adnode.nodeName =  lastOne.nodeName and adnode.updated_at = lastOne.max_updated_at
+        JOIN ex_em_state ex on ex.state = adnode.adhoclamp_status and ex.test_type = testType
+        JOIN node_details As details on details.NodeID = adnode.nodeID and details.NetworkID = adnode.networkID
+        where adnode.adhoclamp_status in ('f','8') and
+        adnode.schedule = 1 and adho.testType COLLATE utf8mb4_general_ci  in (TestTypeOne, TestTypeTwo)
+        and adnode.NetworkID COLLATE utf8mb4_general_ci in (SELECT NetworkID FROM users_network where UserID COLLATE utf8mb4_general_ci  = userIDVal)
+        order by details.nodeName, adnode.test_finished_at desc;
 
 END
