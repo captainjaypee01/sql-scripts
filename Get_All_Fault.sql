0c6c9334cdaaa10d15b31f1db7a794385c065422
John Paul B Dala <jaypeedala31@gmail.com>
2022-11-15 13:12:17 +0800

    fix showing latest alarm

--- Get_All_Fault.sql
+++ Get_All_Fault.sql
@@ -11,11 +11,14 @@ BEGIN
 	n.ServiceID, n.NodeType, alarm.Descr As FaultDesc, alarm.ResolvedDescription As ResolvedDesc,
     DATE_FORMAT(CONVERT_TZ(alarm.created_at, '+00:00', timezoneOffset), '%Y-%m-%d %H:%i:%s') AS CreatedDate,
 	ifnull(DATE_FORMAT(CONVERT_TZ(alarm.ResolvedTime, '+00:00', timezoneOffset), '%Y-%m-%d %H:%i:%s'),'-') As ResolvedDate
-	FROM node_details As n 
-	JOIN node_alarm_log As alarm on n.NodeID = alarm.NodeID and n.NetworkID = alarm.NetworkID
+	FROM node_alarm_log As alarm 
+    INNER JOIN (SELECT nodeid, max(created_at) as createdDate FROM node_alarm_log GROUP BY nodeid) alarm2
+		on alarm.NodeID = alarm2.NodeID and alarm.created_at = alarm2.createdDate
+	JOIN node_details As n on n.NodeID = alarm.NodeID and n.NetworkID = alarm.NetworkID
 	where n.NodeType In (NodeTypeValOne,NodeTypeValTwo,NodeTypeValThree) and alarm.IsResolved is null 
 	and n.Status = 'Active'
-	and n.NetworkID in (SELECT n.NetworkID FROM users_network un where un.UserID = userIDVal)
+	and alarm.NetworkID in (SELECT NetworkID FROM users_network un where un.UserID = userIDVal)
+    group by alarm.NodeID
 	order by alarm.created_at desc
 	LIMIT 500;
 
