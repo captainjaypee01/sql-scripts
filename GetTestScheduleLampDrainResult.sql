9c3e64d0351054347dddc9a573fe57807b75ce77
John Paul B Dala <jaypeedala31@gmail.com>
2022-11-25 18:02:26 +0800

    GetTestScheduleLampDrainResultInit

--- GetTestScheduleLampDrainResult.sql
+++ GetTestScheduleLampDrainResult.sql
@@ -6,18 +6,20 @@ ScheduleVal varchar(255)
 )
 BEGIN
 
-    SELECT details.nodeName as nodename, ex.state_label as testresults, details.nodeType as teston, details.BuildingName As location, details.BuildingLevel As level, details.SectorName As sector, 
+    
+    SELECT distinct details.nodeName as nodename, ex.state_label as testresults, details.nodeType as teston, details.BuildingName As location, details.BuildingLevel As level, details.SectorName As sector, 
         CONVERT_TZ(adnode.test_finished_at,'+00:00','+8:00') As test_finished_at,
-        CONVERT_TZ(adho.next_schedule_date,'+00:00','+8:00') As next_schedule_date
-        FROM exem_test_schedule As adho 
-        JOIN exem_test_result As adnode on adnode.schedule_no = adho.id and adnode.test_type = testType
-        JOIN (SELECT nodeName, max(updated_at) As max_updated_at FROM (select * from exem_test_result where schedule = 1) As maxup group by nodeName) As lastOne
-        on adnode.nodeName =  lastOne.nodeName and adnode.updated_at = lastOne.max_updated_at
-        JOIN ex_em_state ex on ex.state = adnode.adhoclamp_status and ex.test_type = testType
-        JOIN node_details As details on details.NodeID = adnode.nodeID and details.NetworkID = adnode.networkID
-        where adnode.adhoclamp_status in ('f','8') and
-        adnode.schedule = 1 and adho.testType COLLATE utf8mb4_general_ci  in (TestTypeOne, TestTypeTwo)
-        and adnode.NetworkID COLLATE utf8mb4_general_ci in (SELECT NetworkID FROM users_network where UserID COLLATE utf8mb4_general_ci  = userIDVal)
-        order by details.nodeName, adnode.test_finished_at desc;
+        CONVERT_TZ(adnode.next_schedule_date,'+00:00','+8:00') As next_schedule_date
+    FROM exem_test_schedule As adho 
+    JOIN exem_test_result As adnode on adnode.schedule_no = adho.id and adnode.test_type = testType
+    JOIN (SELECT nodeName, max(created_at) As max_created_at FROM (select * from exem_test_result where schedule = 1 and test_type COLLATE utf8mb4_general_ci  in (TestTypeOne, TestTypeTwo)) As maxup group by nodeName) As lastOne
+        on adnode.nodeName =  lastOne.nodeName and adnode.created_at = lastOne.max_created_at
+    JOIN ex_em_state ex on ex.state = adnode.adhoclamp_status and ex.test_type = testType
+    JOIN node_details As details on details.NodeID = adnode.nodeID and details.NetworkID = adnode.networkID
+    where adnode.schedule = 1 and adho.testType COLLATE utf8mb4_general_ci  in (TestTypeOne, TestTypeTwo)
+    and adnode.adhoclamp_status Not In ('5','6')
+    and adho.schedule_status
+    group by adnode.NodeID
+    order by details.nodeName, adnode.test_finished_at desc;
 
 END
--- GetTestScheduleLampDrainResultInit.sql
+++ GetTestScheduleLampDrainResultInit.sql
@@ -7,17 +7,18 @@ ScheduleVal varchar(255)
 BEGIN
 
     SELECT distinct details.nodeName as nodename, ex.state_label as testresults, details.nodeType as teston, details.BuildingName As location, details.BuildingLevel As level, details.SectorName As sector, 
-    CONVERT_TZ(adnode.test_finished_at,'+00:00','+8:00') As test_finished_at,
-    CONVERT_TZ(adnode.next_schedule_date,'+00:00','+8:00') As next_schedule_date
-    FROM smartnew.exem_test_schedule As adho 
-    JOIN smartnew.exem_test_result As adnode on adnode.schedule_no = adho.id and adnode.test_type = testType
-    JOIN (SELECT nodeName, max(created_at) As max_updated_at FROM (select * from smartnew.exem_test_result where schedule = 1 and test_type COLLATE utf8mb4_general_ci  in (TestTypeOne, TestTypeTwo)) As maxup group by nodeName) As lastOne
-    on adnode.nodeName =  lastOne.nodeName and adnode.updated_at = lastOne.max_updated_at
-    JOIN smartnew.ex_em_state ex on ex.state = adnode.adhoclamp_status and ex.test_type = testType
-    JOIN smartnew.node_details As details on details.NodeID = adnode.nodeID and details.NetworkID = adnode.networkID
+        CONVERT_TZ(adnode.test_finished_at,'+00:00','+8:00') As test_finished_at,
+        CONVERT_TZ(adnode.next_schedule_date,'+00:00','+8:00') As next_schedule_date
+    FROM exem_test_schedule As adho 
+    JOIN exem_test_result As adnode on adnode.schedule_no = adho.id and adnode.test_type = testType
+    JOIN (SELECT nodeName, max(created_at) As max_created_at FROM (select * from exem_test_result where schedule = 1 and test_type COLLATE utf8mb4_general_ci  in (TestTypeOne, TestTypeTwo)) As maxup group by nodeName) As lastOne
+        on adnode.nodeName =  lastOne.nodeName and adnode.created_at = lastOne.max_created_at
+    JOIN ex_em_state ex on ex.state = adnode.adhoclamp_status and ex.test_type = testType
+    JOIN node_details As details on details.NodeID = adnode.nodeID and details.NetworkID = adnode.networkID
     where adnode.schedule = 1 and adho.testType COLLATE utf8mb4_general_ci  in (TestTypeOne, TestTypeTwo)
-    and adnode.adhoclamp_status Not In ('0','5','6')
+    and adnode.adhoclamp_status Not In ('5','6')
     and adho.schedule_status
+    group by adnode.NodeID
     order by details.nodeName, adnode.test_finished_at desc;
 
 END
