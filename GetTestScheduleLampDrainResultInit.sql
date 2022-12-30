42427a8edb28320f25bfb531c3c9a21cc31306bd
John Paul B Dala <jaypeedala31@gmail.com>
2022-12-30 15:27:20 +0800

    update test schedule result list

--- GetTestScheduleLampDrainResultInit.sql
+++ GetTestScheduleLampDrainResultInit.sql
@@ -16,12 +16,11 @@ BEGIN
     JOIN exem_test_result As adnode on adnode.schedule_no = adho.id and adnode.test_type = testType
     JOIN (SELECT nodeName, max(created_at) As max_created_at FROM (select * from exem_test_result where schedule = 1 and test_type COLLATE utf8mb4_general_ci  in (TestTypeOne, TestTypeTwo)) As maxup group by nodeName order by created_at desc) As lastOne
         on adnode.nodeName =  lastOne.nodeName and adnode.created_at = lastOne.max_created_at
-    JOIN ex_em_state ex on ex.state = adnode.adhoclamp_status and ex.test_type = testType
-    LEFT JOIN ex_em_state ex2 on ex2.state = adnode.last_adhoclamp_status and ex2.test_type = testType
+    left JOIN ex_em_state ex on ex.state = adnode.adhoclamp_status and ex.test_type = testType
+    right JOIN ex_em_state ex2 on ex2.state = adnode.last_adhoclamp_status and ex2.test_type = testType
     JOIN node_details As details on details.NodeID = adnode.nodeID and details.NetworkID = adnode.networkID
     where adnode.schedule = 1 and adho.testType COLLATE utf8mb4_general_ci  in (TestTypeOne, TestTypeTwo)
     and adnode.adhoclamp_status Not In ('5','6')
-    #and adnode.schedule_status = 1 and adho.schedule_status = 1
     group by adnode.NodeID
     order by adnode.adhoclamp_status, details.nodeName, adnode.test_finished_at desc;
 
